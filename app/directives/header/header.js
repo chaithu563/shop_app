f4f9239438a1a420971b91650e0017693818736a
chaitanya edara <chaitanya.eedara@gmail.com>
2015-08-09 21:00:01 +0530

    css design added

---
+++ app/directives/header/header.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="header.html" />
+angular.module('myApp.directives', [])
+
+.directive('shopHeader', ['$compile', function ($compile) {
+
+    return {
+
+        templateUrl: 'header.html',
+        restrict: 'E'
+    
+    }
+
+
+}]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,9 +10,9 @@ angular.module('myApp.view1', ['ui.router'])
 
     var items = [
                 { id: 1, name: "Blazer",ST:11, ET:13, imgURL: "http://www.saymoontailors.com/uploads/21.jpg", redURL: blazerItem },
-                { id: 2, name: "Cap",ST:40, ET:41, imgURL: "http://www.volkl.com/uploads/tx_voelkldata/fashion/VolklLogoCap_BLK_L_01.png", redURL: capItem },
+                { id: 2, name: "Cap",ST:38, ET:41, imgURL: "http://www.volkl.com/uploads/tx_voelkldata/fashion/VolklLogoCap_BLK_L_01.png", redURL: capItem },
                 { id: 3, name: "car",ST:86, ET:87, imgURL: "http://cdn.wonderfulengineering.com/wp-content/uploads/2013/11/bmw-cars-picture.jpg", redURL: carItem },
-                { id: 3, name: "Dinning",ST:34, ET:40, imgURL: "http://i1.pepperfry.com/media/catalog/product/t/w/800x880/two-seater-dining-set-in-white-colour-by-ventura-two-seater-dining-set-in-white-colour-by-ventura-mpm4hn.jpg", redURL: DiningItem },
+                { id: 3, name: "Dinning",ST:34, ET:41, imgURL: "http://i1.pepperfry.com/media/catalog/product/t/w/800x880/two-seater-dining-set-in-white-colour-by-ventura-two-seater-dining-set-in-white-colour-by-ventura-mpm4hn.jpg", redURL: DiningItem },
                 { id: 3, name: "cycle", ST:118, ET:145,imgURL: "http://imshopping.rediff.com/imgshop/800-1280/shopping/pixs/18320/c/cycle_54a3b8fe76041._hero-electric-avior-amx-electric-cycle.jpg", redURL: CycleItem }
 
                 ];
@@ -42,10 +42,10 @@ angular.module('myApp.view1', ['ui.router'])
     }
     //document.getElementById("MyVideo1").onplay(vidplay())
 
-    $scope.vidplay=function() {
-
-        $scope.isVisible = false;;
-
-    }
+   
+    document.getElementById("MyVideo1").onplaying = function () {
+        $scope.isVisible = false;
+        $scope.$apply();
+    };
 
 }]);
