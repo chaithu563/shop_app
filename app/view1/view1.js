a8dc48c74a7fe721185a93bc24f702aae8401e87
chaitanya edara <chaitanya.eedara@gmail.com>
2015-08-11 09:46:17 +0530

    css changes suggested

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -19,12 +19,7 @@ angular.module('myApp.view1', ['ui.router'])
 
     $scope.isVisible = false;
 
-    $scope.ShowItemsAvail = function (item) {
-        document.getElementById('MyVideo1').pause();
-
-        $scope.availItems = getAvailableItems();
-        $scope.isVisible = true;
-    }
+  
 
     var getAvailableItems = function () {
 
@@ -43,9 +38,161 @@ angular.module('myApp.view1', ['ui.router'])
     //document.getElementById("MyVideo1").onplay(vidplay())
 
    
-    document.getElementById("MyVideo1").onplaying = function () {
-        $scope.isVisible = false;
-        $scope.$apply();
-    };
+  
+
+
+
+    //search handle
+
+    $scope.videos = [
+           { name: 'Attharinti Daredi', code: '1' },
+           { name: 'Aagadu', code: '2' },
+           { name: 'Athadu', code: '3' },
+           { name: 'Avakay Biryani', code: '4' },
+   
+           { name: 'Bhartiyudu', code: '5' },
+           { name: 'Bava', code: '6' },
+           { name: 'Bhuzzigadu', code: '7' },
+          
+           { name: 'Chinnadanee', code: '8' },
+           { name: 'Chukkalo Chnadrudu', code: '9' },
+          
+           { name: 'Dadi', code: '10' },
+           { name: 'Don', code: '11' },
+           { name: 'Doosukosta', code: '12' },
+          
+          
+           { name: 'Gamyam', code: '13' },
+           { name: 'Godavari', code: '15' },
+
+          
+         
+         
+           { name: 'Jilla', code: '16' },
+           { name: 'Gabbar Sing', code: '18' },
+          
+           { name: 'Munna', code: '19' },
+          
+           { name: '3Idiots', code: '20' },
+           { name: 'Businessman', code: '21' },
+           
+          
+           { name: 'Sreemanthudu', code: '22' },
+           { name: 'Nenokkadee', code: '23' },
+          
+           { name: 'Bahubali', code: '24' }
+           
+    
+    ];
+    $scope.selectedVideo = '22';
+
+    //video control
+        // Video
+        var video = document.getElementById("MyVideo1");
+
+        // Buttons
+        var playButton = document.getElementById("play-pause");
+        var muteButton = document.getElementById("mute");
+        var fullScreenButton = document.getElementById("full-screen");
+
+        // Sliders
+        var seekBar = document.getElementById("seek-bar");
+        var volumeBar = document.getElementById("volume-bar");
+
+
+        // Event listener for the play/pause button
+        playButton.addEventListener("click", function () {
+            if (video.paused == true) {
+                // Play the video
+                video.play();
+
+                // Update the button text to 'Pause'
+                playButton.innerHTML = "Pause";
+            } else {
+                // Pause the video
+                video.pause();
+
+                // Update the button text to 'Play'
+                playButton.innerHTML = "Play";
+            }
+        });
+
+        document.getElementById("MyVideo1").onplaying = function () {
+            $scope.isVisible = false;
+            $scope.$apply();
+        };
+        $scope.ShowItemsAvail = function (item) {
+            video.pause();
+            // Update the button text to 'Play'
+            playButton.innerHTML = "Play";
+            $scope.availItems = getAvailableItems();
+            $scope.isVisible = true;
+        }
+
+
+        // Event listener for the mute button
+        muteButton.addEventListener("click", function () {
+            if (video.muted == false) {
+                // Mute the video
+                video.muted = true;
+
+                // Update the button text
+                muteButton.innerHTML = "Unmute";
+            } else {
+                // Unmute the video
+                video.muted = false;
+
+                // Update the button text
+                muteButton.innerHTML = "Mute";
+            }
+        });
+
+
+        // Event listener for the full-screen button
+        fullScreenButton.addEventListener("click", function () {
+            if (video.requestFullscreen) {
+                video.requestFullscreen();
+            } else if (video.mozRequestFullScreen) {
+                video.mozRequestFullScreen(); // Firefox
+            } else if (video.webkitRequestFullscreen) {
+                video.webkitRequestFullscreen(); // Chrome and Safari
+            }
+        });
+
+
+        // Event listener for the seek bar
+        seekBar.addEventListener("change", function () {
+            // Calculate the new time
+            var time = video.duration * (seekBar.value / 100);
+
+            // Update the video time
+            video.currentTime = time;
+        });
+
+
+        // Update the seek bar as the video plays
+        video.addEventListener("timeupdate", function () {
+            // Calculate the slider value
+            var value = (100 / video.duration) * video.currentTime;
+
+            // Update the slider value
+            seekBar.value = value;
+        });
+
+        // Pause the video when the seek handle is being dragged
+        seekBar.addEventListener("mousedown", function () {
+            video.pause();
+        });
+
+        // Play the video when the seek handle is dropped
+        seekBar.addEventListener("mouseup", function () {
+            video.play();
+        });
+
+        // Event listener for the volume bar
+        volumeBar.addEventListener("change", function () {
+            // Update the video volume
+            video.volume = volumeBar.value;
+        });
 
 }]);
