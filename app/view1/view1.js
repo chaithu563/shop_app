4980d7888520d7798b1fe2be25726975cfda0466
chaitanya edara <chaitanya.eedara@gmail.com>
2015-08-13 09:24:05 +0530

    changes new items added

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,19 +1,23 @@
 'use strict';
 angular.module('myApp.view1', ['ui.router'])
 
-.controller('View1Ctrl', ['$state', '$scope', 'dataServices', '$http', '$window','_', function ($state, $scope, dataServices, $http, $window,_) {
-	var blazerItem = "http://www.flipkart.com/protext-premium-solid-single-breasted-formal-men-s-blazer/p/itme5sh4tfxhf8hg?pid=BZRE5SH4C8PQMMWM&ref=L%3A-2203174991299721304&srno=p_31&query=blazer&otracker=from-search";
-	var carItem = "http://www.zigwheels.com/newcars/Hyundai/Creta";
-	var capItem = "http://www.flipkart.com/cravers-premium-solid-snapback-cap/p/itme5tpvrzmkgq6n?pid=CAPE5TPVUUCMADYY&ref=L%3A7670444542371712963&srno=p_2&query=cap&otracker=from-search";
-	var DiningItem = "http://www.pepperfry.com/two-seater-dining-set-in-white-colour-by-ventura-1265359.html?type=sel";
-	var CycleItem = "http://www.snapdeal.com/product/btwin-my-bike-7s-mountain/466642201#bcrumbLabelId:280";
-	var glassItem = "http://www.snapdeal.com/product/krm-moonstone-straight-shelf-10x5/100199843#bcrumbSearch:glass";
-	var tableItem = "http://www.snapdeal.com/product/study-table-with-3-drawers/884763667";
-	var chairItem = "http://www.snapdeal.com/product/divano-modular-black-metal-office/1301325749";
-	var cupItem = "http://www.amazon.com/Stainless-Steel-Sauce-Condiment-Cups/dp/B000K9WTN2";
-	var shirtItem = "http://www.amazon.com/Russell-Athletic-Basic-T-Shirt-XXX-Large/dp/B00076ZRC8/ref=sr_1_1?ie=UTF8&qid=1439295413&sr=8-1&keywords=t+shirt";
-	var items = [
-                { id: 1, name: "Blazer", ST: 11, ET: 13, imgURL: "http://www.saymoontailors.com/uploads/21.jpg", redURL: blazerItem,price:10000 },
+.controller('View1Ctrl', ['$state', '$scope', 'dataServices', '$http', '$window', '_', function ($state, $scope, dataServices, $http, $window, _) {
+    var blazerItem = "http://www.flipkart.com/protext-premium-solid-single-breasted-formal-men-s-blazer/p/itme5sh4tfxhf8hg?pid=BZRE5SH4C8PQMMWM&ref=L%3A-2203174991299721304&srno=p_31&query=blazer&otracker=from-search";
+    var carItem = "http://www.zigwheels.com/newcars/Hyundai/Creta";
+    var capItem = "http://www.flipkart.com/cravers-premium-solid-snapback-cap/p/itme5tpvrzmkgq6n?pid=CAPE5TPVUUCMADYY&ref=L%3A7670444542371712963&srno=p_2&query=cap&otracker=from-search";
+    var DiningItem = "http://www.pepperfry.com/two-seater-dining-set-in-white-colour-by-ventura-1265359.html?type=sel";
+    var CycleItem = "http://www.snapdeal.com/product/btwin-my-bike-7s-mountain/466642201#bcrumbLabelId:280";
+    var glassItem = "http://www.snapdeal.com/product/krm-moonstone-straight-shelf-10x5/100199843#bcrumbSearch:glass";
+    var tableItem = "http://www.snapdeal.com/product/study-table-with-3-drawers/884763667";
+    var chairItem = "http://www.snapdeal.com/product/divano-modular-black-metal-office/1301325749";
+    var cupItem = "http://www.amazon.com/Stainless-Steel-Sauce-Condiment-Cups/dp/B000K9WTN2";
+    var shirtItem = "http://www.amazon.com/Russell-Athletic-Basic-T-Shirt-XXX-Large/dp/B00076ZRC8/ref=sr_1_1?ie=UTF8&qid=1439295413&sr=8-1&keywords=t+shirt";
+    var tileItem = "http://www.snapdeal.com/product/styleon-multicolour-glass-tile/624025876158";
+    var spectItem = "http://www.lenskart.com/vincent-chase-vc-0315-gunmetal-grey-black-doy2-eyeglasses.html";
+    var wallDocrItem = "http://www.snapdeal.com/product/aquire-pvc-wall-stickers/638522664252";
+
+    var items = [
+                { id: 1, name: "Blazer", ST: 11, ET: 13, imgURL: "http://www.saymoontailors.com/uploads/21.jpg", redURL: blazerItem, price: 10000 },
                 { id: 2, name: "Cap", ST: 39, ET: 41, imgURL: "http://www.volkl.com/uploads/tx_voelkldata/fashion/VolklLogoCap_BLK_L_01.png", redURL: capItem, price: 1000 },
                 { id: 3, name: "car", ST: 86, ET: 87, imgURL: "http://cdn.wonderfulengineering.com/wp-content/uploads/2013/11/bmw-cars-picture.jpg", redURL: carItem, price: 1000000 },
                 { id: 4, name: "Dinning", ST: 35, ET: 41, imgURL: "http://i1.pepperfry.com/media/catalog/product/t/w/800x880/two-seater-dining-set-in-white-colour-by-ventura-two-seater-dining-set-in-white-colour-by-ventura-mpm4hn.jpg", redURL: DiningItem, price: 19000 },
@@ -28,16 +32,24 @@ angular.module('myApp.view1', ['ui.router'])
 				  { id: 13, name: "Cup", ST: 18, ET: 24, imgURL: "http://images.wisegeek.com/cup-of-white-sauce.jpg", redURL: cupItem, price: 1000 },
 				   { id: 14, name: "Cup", ST: 35, ET: 41, imgURL: "http://images.wisegeek.com/cup-of-white-sauce.jpg", redURL: cupItem, price: 1000 },
 				    { id: 15, name: "Shirt", ST: 42, ET: 56, imgURL: "http://www.crunchwear.com/wp-content/uploads/2013/08/Personal-Soundtrack-shirt.jpg", redURL: shirtItem, price: 1000 },
-					{ id: 16, name: "Shirt", ST: 73, ET: 82, imgURL: "http://www.crunchwear.com/wp-content/uploads/2013/08/Personal-Soundtrack-shirt.jpg", redURL: shirtItem, price: 1000 }
-	];
+					{ id: 16, name: "Shirt", ST: 73, ET: 82, imgURL: "http://www.crunchwear.com/wp-content/uploads/2013/08/Personal-Soundtrack-shirt.jpg", redURL: shirtItem, price: 1000 },
+	      { id: 17, name: "Shirt", ST: 18, ET: 24, imgURL: "http://www.crunchwear.com/wp-content/uploads/2013/08/Personal-Soundtrack-shirt.jpg", redURL: shirtItem, price: 1000 },
+               { id: 18, name: "Shirt", ST: 14, ET: 15, imgURL: "http://www.crunchwear.com/wp-content/uploads/2013/08/Personal-Soundtrack-shirt.jpg", redURL: shirtItem, price: 1000 },
+                { id: 19, name: "Tiles", ST: 14, ET: 15, imgURL: "http://hardwoodfloorscharlotte.com/wp-content/uploads/2014/02/plymouth-brown.jpg", redURL: tileItem, price: 1000 },
+                 { id: 20, name: "Spectales", ST: 18, ET: 21, imgURL: "http://www.firmoo.com/frame-images/1364/middle-unisex-plastic-eyeglasses-6485.png", redURL: spectItem, price: 1000 },
+                  { id: 21, name: "Shirt", ST: 39, ET: 41, imgURL: "http://www.crunchwear.com/wp-content/uploads/2013/08/Personal-Soundtrack-shirt.jpg", redURL: shirtItem, price: 1000 },
+
+
+                  { id: 22, name: "Wall Decoration", ST: 35, ET: 37, imgURL: "http://bestinspired.com/wp-content/uploads/2015/04/Wall-Decor.jpg", redURL: wallDocrItem, price: 140000 }
+    ];
 
     $scope.isVisible = false;
 
-  
+
 
     var getAvailableItems = function () {
 
-        var curTime=Math.floor(document.getElementById('MyVideo1').currentTime);
+        var curTime = Math.floor(document.getElementById('MyVideo1').currentTime);
         var filteredItems = items;
         filteredItems = _.filter(filteredItems, function (item) {
             return (item.ST <= curTime && item.ET >= curTime);
@@ -51,8 +63,8 @@ angular.module('myApp.view1', ['ui.router'])
     }
     //document.getElementById("MyVideo1").onplay(vidplay())
 
-   
-  
+
+
 
 
 
@@ -63,167 +75,167 @@ angular.module('myApp.view1', ['ui.router'])
            { name: 'Aagadu', code: '2' },
            { name: 'Athadu', code: '3' },
            { name: 'Avakay Biryani', code: '4' },
-   
+
            { name: 'Bhartiyudu', code: '5' },
            { name: 'Bava', code: '6' },
            { name: 'Bhuzzigadu', code: '7' },
-          
+
            { name: 'Chinnadanee', code: '8' },
            { name: 'Chukkalo Chnadrudu', code: '9' },
-          
+
            { name: 'Dadi', code: '10' },
            { name: 'Don', code: '11' },
            { name: 'Doosukosta', code: '12' },
-          
-          
+
+
            { name: 'Gamyam', code: '13' },
            { name: 'Godavari', code: '15' },
 
-          
-         
-         
+
+
+
            { name: 'Jilla', code: '16' },
            { name: 'Gabbar Sing', code: '18' },
-          
+
            { name: 'Munna', code: '19' },
-          
+
            { name: '3Idiots', code: '20' },
            { name: 'Businessman', code: '21' },
-           
-          
+
+
            { name: 'Sreemanthudu', code: '22' },
            { name: 'Nenokkadee', code: '23' },
-          
+
            { name: 'Bahubali', code: '24' }
-           
-    
+
+
     ];
     $scope.selectedVideo = '22';
 
     //video control
-        // Video
-        var video = document.getElementById("MyVideo1");
-        video.play();
-        $scope.playorpause = "pause";
-        $scope.muteorunmute = "speaker-on";
-        // Buttons
-        var playButton = document.getElementById("play-pause");
-        var muteButton = document.getElementById("mute");
-        var fullScreenButton = document.getElementById("full-screen");
+    // Video
+    var video = document.getElementById("MyVideo1");
+    video.play();
+    $scope.playorpause = "pause";
+    $scope.muteorunmute = "speaker-on";
+    // Buttons
+    var playButton = document.getElementById("play-pause");
+    var muteButton = document.getElementById("mute");
+    var fullScreenButton = document.getElementById("full-screen");
 
     // Sliders
-        var seektime=document.getElementById("seek-time");
-        var seekBar = document.getElementById("seek-bar");
-        var volumeBar = document.getElementById("volume-bar");
+    var seektime = document.getElementById("seek-time");
+    var seekBar = document.getElementById("seek-bar");
+    var volumeBar = document.getElementById("volume-bar");
 
 
-        // Event listener for the play/pause button
-        playButton.addEventListener("click", function () {
-            if (video.paused == true) {
-                // Play the video
-                video.play();
-                $scope.playorpause = "pause";
-               
-                // Update the button text to 'Pause'
-              //  playButton.innerHTML = "Pause";
-            } else {
-                // Pause the video
-                video.pause();
-                $scope.playorpause = "play";
-              
-                // Update the button text to 'Play'
-               // playButton.innerHTML = "Play";
-            }
-            $scope.opacityinorout = "out";
-            $scope.$apply();
-        });
+    // Event listener for the play/pause button
+    playButton.addEventListener("click", function () {
+        if (video.paused == true) {
+            // Play the video
+            video.play();
+            $scope.playorpause = "pause";
 
-        document.getElementById("MyVideo1").onplaying = function () {
-            $scope.isVisible = false;
-            $scope.$apply();
-        };
-        $scope.ShowItemsAvail = function (item) {
+            // Update the button text to 'Pause'
+            //  playButton.innerHTML = "Pause";
+        } else {
+            // Pause the video
             video.pause();
             $scope.playorpause = "play";
+
             // Update the button text to 'Play'
-          //  playButton.innerHTML = "Play";
-            $scope.availItems = getAvailableItems();
-            $scope.isVisible = true;
-            $scope.opacityinorout = "in";
-            $scope.$apply();
+            // playButton.innerHTML = "Play";
+        }
+        $scope.opacityinorout = "out";
+        $scope.$apply();
+    });
+
+    document.getElementById("MyVideo1").onplaying = function () {
+        $scope.isVisible = false;
+        $scope.$apply();
+    };
+    $scope.ShowItemsAvail = function (item) {
+        video.pause();
+        $scope.playorpause = "play";
+        // Update the button text to 'Play'
+        //  playButton.innerHTML = "Play";
+        $scope.availItems = getAvailableItems();
+        $scope.isVisible = true;
+        $scope.opacityinorout = "in";
+        $scope.$apply();
+    }
+
+
+    // Event listener for the mute button
+    muteButton.addEventListener("click", function () {
+        if (video.muted == false) {
+            // Mute the video
+            video.muted = true;
+
+            // Update the button text
+            // muteButton.innerHTML = "Unmute";
+            $scope.muteorunmute = "mute";
+        } else {
+            // Unmute the video
+            video.muted = false;
+
+            // Update the button text
+            //muteButton.innerHTML = "Mute";
+            $scope.muteorunmute = "speaker-on";
         }
 
-
-        // Event listener for the mute button
-        muteButton.addEventListener("click", function () {
-            if (video.muted == false) {
-                // Mute the video
-                video.muted = true;
-
-                // Update the button text
-                // muteButton.innerHTML = "Unmute";
-                $scope.muteorunmute = "mute";
-            } else {
-                // Unmute the video
-                video.muted = false;
-
-                // Update the button text
-                //muteButton.innerHTML = "Mute";
-                $scope.muteorunmute = "speaker-on";
-            }
-
-            $scope.$apply();
-        });
-
-
-        // Event listener for the full-screen button
-        fullScreenButton.addEventListener("click", function () {
-            if (video.requestFullscreen) {
-                video.requestFullscreen();
-            } else if (video.mozRequestFullScreen) {
-                video.mozRequestFullScreen(); // Firefox
-            } else if (video.webkitRequestFullscreen) {
-                video.webkitRequestFullscreen(); // Chrome and Safari
-            }
-        });
-
-
-        // Event listener for the seek bar
-        seekBar.addEventListener("change", function () {
-            // Calculate the new time
-            var time = video.duration * (seekBar.value / 100);
-
-            // Update the video time
-            video.currentTime = time;
-           
-        });
-
-
-        // Update the seek bar as the video plays
-        video.addEventListener("timeupdate", function () {
-            // Calculate the slider value
-            var value = (100 / video.duration) * video.currentTime;
-
-            // Update the slider value
-            seekBar.value = value;
-            seektime.textContent = Math.floor(video.currentTime);
-        });
-
-        // Pause the video when the seek handle is being dragged
-        seekBar.addEventListener("mousedown", function () {
-            video.pause();
-        });
-
-        // Play the video when the seek handle is dropped
-        seekBar.addEventListener("mouseup", function () {
-            video.play();
-        });
-
-        // Event listener for the volume bar
-        volumeBar.addEventListener("change", function () {
-            // Update the video volume
-            video.volume = volumeBar.value;
-        });
         $scope.$apply();
+    });
+
+
+    // Event listener for the full-screen button
+    fullScreenButton.addEventListener("click", function () {
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen(); // Firefox
+        } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen(); // Chrome and Safari
+        }
+    });
+
+
+    // Event listener for the seek bar
+    seekBar.addEventListener("change", function () {
+        // Calculate the new time
+        var time = video.duration * (seekBar.value / 100);
+
+        // Update the video time
+        video.currentTime = time;
+
+    });
+
+
+    // Update the seek bar as the video plays
+    video.addEventListener("timeupdate", function () {
+        // Calculate the slider value
+        var value = (100 / video.duration) * video.currentTime;
+
+        // Update the slider value
+        seekBar.value = value;
+        seektime.textContent = Math.floor(video.currentTime);
+    });
+
+    // Pause the video when the seek handle is being dragged
+    seekBar.addEventListener("mousedown", function () {
+        video.pause();
+    });
+
+    // Play the video when the seek handle is dropped
+    seekBar.addEventListener("mouseup", function () {
+        video.play();
+    });
+
+    // Event listener for the volume bar
+    volumeBar.addEventListener("change", function () {
+        // Update the video volume
+        video.volume = volumeBar.value;
+    });
+    $scope.$apply();
 
 }]);
