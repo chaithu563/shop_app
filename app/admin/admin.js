f40c87de7d727333ad3bd99616c467180318a90a
chaitanya edara <chaitanya.eedara@gmail.com>
2015-09-03 21:16:13 +0530

    admin page input added

--- app/admin/admin.js
+++ app/admin/admin.js
@@ -3,6 +3,7 @@ angular.module('myApp.admin', ['ui.router'])
 
 .controller('adminCtrl', ['$state', '$scope', 'dataServices', '$http', '$window', '_', '$timeout', '$interval', function ($state, $scope, dataServices, $http, $window, _, $timeout, $interval) {
   
+    //{ id: 6, name: "cycle", x: 56, y: 40, ST: 9, ET: 10, imgURL: "cycle.jpg", redURL: CycleItem, price: 29000 },
 
 
 }]);
