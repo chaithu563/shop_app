7c7d37e14de17f4d6f92620b9816167f45f26956
chaitanya edara <chaitanya.eedara@gmail.com>
2015-10-08 20:23:12 +0530

    Happi pug service

---
+++ HappiPugService/HappiPugService/Controllers/MovieItemsController.cs
@@ -0,0 +1,43 @@
+﻿using HappiPugService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HappiPugService.Controllers
+{
+    public class MovieItemsController : ApiController
+    {
+        // GET api/values
+        happipugEntities ob = new happipugEntities();
+        public IEnumerable<MovieItem> Get()
+        {
+            return ob.MovieItems;
+        }
+
+        // GET api/values/5
+        public MovieItem Get(int id)
+        {
+            return ob.MovieItems.Single(x => x.Id == id);
+        }
+
+        // POST api/values
+        public void Post(MovieItem item)
+        {
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, MovieItem value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
---
+++ HappiPugService/HappiPugService/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HappiPugService.Models;
+namespace HappiPugService.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        // GET api/values
+        happipugEntities ob = new happipugEntities();
+        public IEnumerable<Movie> Get()
+        {
+            return ob.Movies;
+        }
+
+        // GET api/values/5
+        public Movie Get(int id)
+        {
+            return ob.Movies.Single(x=>x.Id==id);
+        }
+
+        // POST api/values
+        public void Post(Movie item)
+        {
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, Movie value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        
+        }
+    }
+}
