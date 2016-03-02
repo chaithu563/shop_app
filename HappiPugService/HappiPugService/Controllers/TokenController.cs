14ae4e64fa2e47fadec453132f60305af79ec67b
chaitanya edara <chaitanya.edara@aveva.com>
2016-03-02 17:33:59 +0530

    key not working

---
+++ HappiPugService/HappiPugService/Controllers/TokenController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace HappiPugService.Controllers
+{
+    public class TokenController : ApiController
+    {
+
+			public dynamic Get()
+			{
+				// certificate
+				var certificate = new X509Certificate2("HappiePug-bbca6989906d.p12", "notasecret", X509KeyStorageFlags.MachineKeySet);
+
+				// header
+				var header = new { typ = "JWT", alg = "RS256" };
+
+				// claimset
+				var times = GetExpiryAndIssueDate();
+				var claimset = new
+				{
+					iss = "happiepug@inlaid-backbone-120410.iam.gserviceaccount.com",
+					scope = "https://www.googleapis.com/auth/prediction",
+					aud = "https://www.googleapis.com/oauth2/v4/token",
+					iat = times[0],
+					exp = times[1],
+				};
+
+				JavaScriptSerializer ser = new JavaScriptSerializer();
+
+				// encoded header
+				var headerSerialized = ser.Serialize(header);
+				var headerBytes = Encoding.UTF8.GetBytes(headerSerialized);
+				var headerEncoded = Convert.ToBase64String(headerBytes);
+
+				// encoded claimset
+				var claimsetSerialized = ser.Serialize(claimset);
+				var claimsetBytes = Encoding.UTF8.GetBytes(claimsetSerialized);
+				var claimsetEncoded = Convert.ToBase64String(claimsetBytes);
+
+				// input
+				var input = headerEncoded + "." + claimsetEncoded;
+				var inputBytes = Encoding.UTF8.GetBytes(input);
+
+				// signiture
+				var rsa = certificate.PrivateKey as RSACryptoServiceProvider;
+				var cspParam = new CspParameters
+				{
+					KeyContainerName = rsa.CspKeyContainerInfo.KeyContainerName,
+					KeyNumber = rsa.CspKeyContainerInfo.KeyNumber == KeyNumber.Exchange ? 1 : 2
+				};
+				var aescsp = new RSACryptoServiceProvider(cspParam) { PersistKeyInCsp = false };
+				var signatureBytes = aescsp.SignData(inputBytes, "SHA256");
+				var signatureEncoded = Convert.ToBase64String(signatureBytes);
+
+				// jwt
+				var jwt = headerEncoded + "." + claimsetEncoded + "." + signatureEncoded;
+
+				var client = new WebClient();
+				client.Encoding = Encoding.UTF8;
+				//var uri = "https://accounts.google.com/o/oauth2/token";
+				var uri = "https://www.googleapis.com/oauth2/v4/token";
+				var content = new NameValueCollection();
+
+				content["assertion"] = jwt;
+				content["grant_type"] = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+
+				string response = Encoding.UTF8.GetString(client.UploadValues(uri, "POST", content));
+
+				var result = ser.Deserialize<dynamic>(response);
+
+				return result;
+			}
+
+			private static int[] GetExpiryAndIssueDate()
+			{
+				var utc0 = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+				var issueTime = DateTime.UtcNow;
+
+				var iat = (int)issueTime.Subtract(utc0).TotalSeconds;
+				var exp = (int)issueTime.AddMinutes(55).Subtract(utc0).TotalSeconds;
+
+				return new[] { iat, exp };
+			}
+    }
+}
