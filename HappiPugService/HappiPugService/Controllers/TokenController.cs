747795ae3ba12366138a2a9972b60887dd90bc85
chaitanya edara <chaitanya.edara@aveva.com>
2016-03-04 16:17:06 +0530

    not working in cloud

--- HappiPugService/HappiPugService/Controllers/TokenController.cs
+++ HappiPugService/HappiPugService/Controllers/TokenController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -16,72 +17,86 @@ namespace HappiPugService.Controllers
     public class TokenController : ApiController
     {
 
-			public string Get()
+			public dynamic Get()
 			{
+				var result = "";
 				// certificate
-					var certificate = new X509Certificate2(HttpContext.Current.Server.MapPath("~/Controllers/HappiePug-3c5325296563.p12"), "notasecret");
-
-			//	var certificate = new X509Certificate2(@"D:\raja\HappiPugService\HappiPugService\Controllers\HappiePug-3c5325296563.p12", "notasecret");
-
-				// header
-				var header = new { typ = "JWT", alg = "RS256" };
-
-				// claimset
-				var times = GetExpiryAndIssueDate();
-				var claimset = new
+				try
 				{
-					iss = "happiepug@inlaid-backbone-120410.iam.gserviceaccount.com",
-					scope = "https://www.googleapis.com/auth/prediction",
-					//scope = "https://www.googleapis.com/auth/devstorage.readonly",
-					aud = "https://www.googleapis.com/oauth2/v4/token",
-					iat = times[0],
-					exp = times[1],
-				};
+					string privateKeyCert = "HappiePug-3c5325296563.p12";
+					string appRoot = Environment.GetEnvironmentVariable("RoleRoot");
+					string pathToPrivateKey = Path.Combine(appRoot + @"\", string.Format(@"approot\{0}", privateKeyCert));
 
-				JavaScriptSerializer ser = new JavaScriptSerializer();
+					var certificate = new X509Certificate2(pathToPrivateKey, "notasecret");
+					//	var certificate = new X509Certificate2(HttpContext.Current.Server.MapPath("~/Controllers/HappiePug-3c5325296563.p12"), "notasecret");
 
-				// encoded header
-				var headerSerialized = ser.Serialize(header);
-				var headerBytes = Encoding.UTF8.GetBytes(headerSerialized);
-				var headerEncoded = Convert.ToBase64String(headerBytes);
+					//	var certificate = new X509Certificate2(@"D:\raja\HappiPugService\HappiPugService\Controllers\HappiePug-3c5325296563.p12", "notasecret");
 
-				// encoded claimset
-				var claimsetSerialized = ser.Serialize(claimset);
-				var claimsetBytes = Encoding.UTF8.GetBytes(claimsetSerialized);
-				var claimsetEncoded = Convert.ToBase64String(claimsetBytes);
+					// header
+					var header = new {typ = "JWT", alg = "RS256"};
 
-				// input
-				var input = headerEncoded + "." + claimsetEncoded;
-				var inputBytes = Encoding.UTF8.GetBytes(input);
+					// claimset
+					var times = GetExpiryAndIssueDate();
+					var claimset = new
+					{
+						iss = "happiepug@inlaid-backbone-120410.iam.gserviceaccount.com",
+						scope = "https://www.googleapis.com/auth/prediction",
+						//scope = "https://www.googleapis.com/auth/devstorage.readonly",
+						aud = "https://www.googleapis.com/oauth2/v4/token",
+						iat = times[0],
+						exp = times[1],
+					};
 
-				// signiture
-				var rsa = certificate.PrivateKey as RSACryptoServiceProvider;
-				var cspParam = new CspParameters
+					JavaScriptSerializer ser = new JavaScriptSerializer();
+
+					// encoded header
+					var headerSerialized = ser.Serialize(header);
+					var headerBytes = Encoding.UTF8.GetBytes(headerSerialized);
+					var headerEncoded = Convert.ToBase64String(headerBytes);
+
+					// encoded claimset
+					var claimsetSerialized = ser.Serialize(claimset);
+					var claimsetBytes = Encoding.UTF8.GetBytes(claimsetSerialized);
+					var claimsetEncoded = Convert.ToBase64String(claimsetBytes);
+
+					// input
+					var input = headerEncoded + "." + claimsetEncoded;
+					var inputBytes = Encoding.UTF8.GetBytes(input);
+
+					// signiture
+					var rsa = certificate.PrivateKey as RSACryptoServiceProvider;
+					var cspParam = new CspParameters
+					{
+						KeyContainerName = rsa.CspKeyContainerInfo.KeyContainerName,
+						KeyNumber = rsa.CspKeyContainerInfo.KeyNumber == KeyNumber.Exchange ? 1 : 2
+					};
+					var aescsp = new RSACryptoServiceProvider(cspParam) {PersistKeyInCsp = false};
+					var signatureBytes = aescsp.SignData(inputBytes, "SHA256");
+					var signatureEncoded = Convert.ToBase64String(signatureBytes);
+
+					// jwt
+					var jwt = headerEncoded + "." + claimsetEncoded + "." + signatureEncoded;
+
+					var client = new WebClient();
+					client.Encoding = Encoding.UTF8;
+					//var uri = "https://accounts.google.com/o/oauth2/token";
+					var uri = "https://www.googleapis.com/oauth2/v4/token";
+					var content = new NameValueCollection();
+
+					content["assertion"] = jwt;
+					content["grant_type"] = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+
+					string response = Encoding.UTF8.GetString(client.UploadValues(uri, "POST", content));
+
+					 result = ser.Deserialize<dynamic>(response);
+				}
+				catch(Exception ex)
 				{
-					KeyContainerName = rsa.CspKeyContainerInfo.KeyContainerName,
-					KeyNumber = rsa.CspKeyContainerInfo.KeyNumber == KeyNumber.Exchange ? 1 : 2
-				};
-				var aescsp = new RSACryptoServiceProvider(cspParam) { PersistKeyInCsp = false };
-				var signatureBytes = aescsp.SignData(inputBytes, "SHA256");
-				var signatureEncoded = Convert.ToBase64String(signatureBytes);
+					result = ex.ToString();
+					return ex;
+				}
 
-				// jwt
-				var jwt = headerEncoded + "." + claimsetEncoded + "." + signatureEncoded;
-
-				var client = new WebClient();
-				client.Encoding = Encoding.UTF8;
-				//var uri = "https://accounts.google.com/o/oauth2/token";
-				var uri = "https://www.googleapis.com/oauth2/v4/token";
-				var content = new NameValueCollection();
-
-				content["assertion"] = jwt;
-				content["grant_type"] = "urn:ietf:params:oauth:grant-type:jwt-bearer";
-
-				string response = Encoding.UTF8.GetString(client.UploadValues(uri, "POST", content));
-
-				var result = ser.Deserialize<dynamic>(response);
-
-				return "chaitanya";
+				return result;
 			}
 
 	    public string Get(int a)
